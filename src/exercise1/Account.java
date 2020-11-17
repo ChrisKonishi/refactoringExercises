b61634005e8384ceda34b496a54d7f3c168f434b
Vitor Horodynski <vitorhorodynski@hotmail.com>
2020-11-17 09:03:03 -0300

    Fixed Account.java

--- src/exercise1/Account.java
+++ src/exercise1/Account.java
@@ -5,30 +5,28 @@ public class Account {
 	// ...
 
 	private int daysOverdrawn;
-
-	public double overdraftCharge() {
-		if (isPremium()) {
-			double result = 10;
-			if (daysOverdrawn > 7) {
-				result += (daysOverdrawn - 7) * 0.85;
-			}
+	private boolean isPremium;
+	private final double BASE_OVERDRAFT_FEE = 4.5
+	private final double PREMIUM_BASE_OVERDRAFT_FEE = 10.0;
+	private final double OVERDRAFT_RATE = 1.75;
+	private final double PREMIUM_OVERDRAFT_RATE = 0.85;
+	private final double PREMIUM_DAYS_TOLERANCE = 7;
+	
+	public double bankCharge() {
+		double result;
+		if (isPremium) {
+			result = PREMIUM_BASE_OVERDRAFT_FEE;
+			if (daysOverdrawn > PREMIUM_DAYS_TOLERANCE)
+				result += (daysOverdrawn - PREMIUM_DAYS_TOLERANCE) * PREMIUM_OVERDRAFT_RATE;
 			return result;
 	    }
-	    else {
-	    	return daysOverdrawn * 1.75;
-	    }
-	}
-	  
-	public double bankCharge() {
-		double result = 4.5;
-		if (daysOverdrawn > 0) {
-			result += overdraftCharge();
-	    }
-	   return result;
+	    else{
+			result = BASE_OVERDRAFT_FEE;
+			return result + daysOverdrawn * OVERDRAFT_RATE;
+		}
 	}
 
 	public boolean isPremium() {
-		// TODO Auto-generated method stub
-		return false;
+		return isPremium;
 	}
 }
