8a1f0d9677b3c6d891aba7ba3d5ace6e067cc8ef
Gabriel de Freitas Garcia <gabrielfreitasgarcia@globo.com>
2020-12-03 10:12:16 -0300

    Exercício feito

--- TDD2/test.py
+++ TDD2/test.py
@@ -1,5 +1,6 @@
 import unittest
 import cache
+import time
 
 class CacheTester(unittest.TestCase):
     def testEmpty(self):
@@ -12,6 +13,29 @@ class CacheTester(unittest.TestCase):
         tupla = cach.getData("peixe.txt")
         self.assertEqual(tupla,("peixe.txt",1))
 
+    def testLimit(self):
+        cach = cache.Cache()
+        tupla = cach.getData("somedaki.txt")
+        tupla = cach.getData("peixe.txt")
+        tupla = cach.getData("santanche.txt")
+        tupla = cach.getData("somedaki.txt")
+        tupla = cach.getData("sugoidesu.txt")
+        testDict = cach.getDict()
+        self.assertDictEqual(testDict,{ "peixe.txt" : 1, "santanche.txt" : 2, "sugoidesu.txt" : 3})
+
+    def testTime(self):
+        cach = cache.Cache()
+        tupla = cach.getData("somedaki.txt")
+        tupla = cach.getData("peixe.txt")
+        tupla = cach.getData("santanche.txt")
+        tupla = cach.getData("somedaki.txt")
+        tupla = cach.getData("sugoidesu.txt")
+        testDict = cach.getDict()
+        self.assertDictEqual(testDict,{ "peixe.txt" : 1, "santanche.txt" : 2, "sugoidesu.txt" : 3})
+        time.sleep(4)
+        tupla = cach.getData("igor.py")
+        testDict = cach.getDict()
+        self.assertDictEqual(testDict, {"igor.py" : 9})
 
 
 if __name__ == '__main__':
